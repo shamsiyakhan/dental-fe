979c2b3f9b06b3780cb0dea270792d188e0955f5
shamsiyakhan <sehrozkhan2704@gmail.com>
2025-05-02 17:43:41 +0530

    omdr ui

--- src/app/omdr-department/allappointment/allappointment.component.ts
+++ src/app/omdr-department/allappointment/allappointment.component.ts
@@ -44,8 +44,44 @@ export class AllappointmentComponent implements OnInit {
       date: '8th Mar, 2025',
       time: '6:00 pm'
     },
+  ]
+
+    progress = [
+      {
+        image: 'assets/doc3.jpg',
+        name: 'Zara Khan',
+        purpose: 'Initial Oral Checkup',
+        id: '980789',
+        date: '8th Mar, 2025',
+        time: '6:00 pm'
+      },
+      {
+        image: 'assets/doc1.jpg',
+        name: 'Aanya Sharma',
+        purpose: 'Medical & Oral History Consultation',
+        id: '890786',
+        date: '8th Mar, 2025',
+        time: '1:00 pm'
+      },
+      {
+        image: 'assets/doc4.jpg',
+        name: 'Aanya Sharma',
+        purpose: 'Medical & Oral History Consultation',
+        id: '890786',
+        date: '8th Mar, 2025',
+        time: '1:00 pm'
+      },
+      {
+        image: 'assets/doc3.jpg',
+        name: 'Zara Khan',
+        purpose: 'Initial Oral Checkup',
+        id: '980789',
+        date: '8th Mar, 2025',
+        time: '6:00 pm'
+      },
     // ... other appointments
   ];
+  
   
   
   selectTab(tab: string) {
